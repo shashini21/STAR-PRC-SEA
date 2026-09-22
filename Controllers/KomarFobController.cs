2a5b4a7d046a3abdad2c025ab0c32d6313594ed4
shashini21 <silvashashini2004@gmail.com>
2026-09-22 17:16:45 +0530

    masterfiles and transaction files UI

---
+++ Controllers/KomarFobController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace STAR_PRC_SEA.Controllers
+{
+    public class KomarFobController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ReferencesControlsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace STAR_PRC_SEA.Controllers
+{
+    public class ReferencesControlsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
